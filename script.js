d6c427fa53011fb56e86b476bbe5ced05f320c2d
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-08-09 22:16:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const container = document.getElementById('container');
 
         function removeColor(element) {
             setTimeout(() => {
-                element.style.backgroundColor =  "rgb(0, 0, 0)";
+                element.style.backgroundColor =  "rgb(29, 29, 29)";
             }, 1000);
         }
 
