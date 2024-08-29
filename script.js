4d902e9989f625a9598123b430c87978b7e579c1
Vishal Kumar Sain <102027742+vis123Git@users.noreply.github.com>
2024-08-29 08:54:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const container = document.getElementById('container');
-        const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D'];
+const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D'];
 
-        for (let i = 0; i < 800; i++) {
-            const square = document.createElement('div');
-            square.classList.add('square');
-            
-            square.addEventListener('mouseenter', () => setColor(square));
-            square.addEventListener('mouseleave', () => removeColor(square));
-            
-            container.appendChild(square);
-        }
+for (let i = 0; i < 800; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    
+    square.addEventListener('mouseenter', () => setColor(square));
+    square.addEventListener('mouseleave', () => removeColor(square));
+    
+    container.appendChild(square);
+}
 
-        function setColor(element) {
-            const color = getRandomColor();
-            element.style.backgroundColor = color;
-        }
+function setColor(element) {
+    const color = getRandomColor();
+    element.style.transition = 'background-color 0s';
+    element.style.backgroundColor = color;
+}
 
-        function removeColor(element) {
-            setTimeout(() => {
-                element.style.backgroundColor =  "rgb(29, 29, 29)";
-            }, 1000);
-        }
+function removeColor(element) {
+    element.style.transition = 'background-color 1s ease';
+    element.style.backgroundColor = 'rgb(29, 29, 29)';
+}
 
-        function getRandomColor() {
-            return colors[Math.floor(Math.random() * colors.length)];
-        }
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+}
